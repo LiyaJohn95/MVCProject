c6c21d64a092b5a6e92a75b1457e3e1d1d94d969
LiyaJohn95 <liyajohn2@gmail.com>
2017-09-09 19:47:16 +0530

    Add files via upload

---
+++ SpringMVCPjct/src/main/java/jbr/springmvc/controller/UserService.java
@@ -0,0 +1,19 @@
+package jbr.springmvc.controller;
+
+import jbr.springmvc.model.Login;
+import jbr.springmvc.model.User;
+
+public class UserService {
+
+	public User validateUser(Login login) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void register(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
